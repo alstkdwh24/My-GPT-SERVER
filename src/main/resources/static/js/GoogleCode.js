ef8fce7fc79a37bc5a664fb078bd3bf103693229
alstkdwh24 <alstkdwh24@naver.com>
2025-07-11 17:52:55 +0900

    ss

--- src/main/resources/static/js/GoogleCode.js
+++ src/main/resources/static/js/GoogleCode.js
@@ -4,11 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let authorizationCode = urlParams.get("code");
     let state = urlParams.get("state");  // 이렇게 하면 state 값을 가져올 수 있습니다.
     console.log("Authorization Code:", authorizationCode);
-    window.console.log = function(message) {
-        // 원래 log 동작 유지하면서...
-        window.flutter_inappwebview.callHandler('consoleLogHandler', message);
-        console.info("[console.log override]:", message);
-    };
+
     $.ajax({
         type: "POST",
         url: "/api/google/getGoogleAccessToken",
@@ -20,6 +16,20 @@ document.addEventListener("DOMContentLoaded", function () {
         success:function(response) {
             let accessToken =response.access_token;
             console.log("Access Token:", accessToken);
+            $.ajax({
+                type:"GET",
+                url: "https://www.googleapis.com/oauth2/v2/userinfo",
+                headers:{
+                    "Authorization": "Bearer " + accessToken
+                },
+                success: function(response) {
+                    console.log("Google User Info:", response);
+                    // response에 name, email, picture 등 정보가 포함됨
+                },
+                error: function(error) {
+                    console.error("Error fetching user info:", error);
+                }
+            })
         }
     })
 })
